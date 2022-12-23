2c63c93d368859a0065c8f4606c863bbfc714299
R89 <rw@RWs-Mac-Pro.local>
2022-12-23 10:46:22 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/usingstackwigit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.usingstackwigit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:usingstackwigit/code.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: code(),
+    ),
+  );
+}
